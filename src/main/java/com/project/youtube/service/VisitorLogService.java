2a7d992e1aa18bcfffc1a86a382f66c9b6245cff
cyseok <cysuk123@naver.com>
2024-07-26 04:28:32 +0900

    사용하지 않은 import 삭제

--- src/main/java/com/project/youtube/service/VisitorLogService.java
+++ src/main/java/com/project/youtube/service/VisitorLogService.java
@@ -1,7 +1,6 @@
 package com.project.youtube.service;
 
 import com.project.youtube.Repository.VisitorLogRepository;
-import com.project.youtube.dto.SearchLog;
 import com.project.youtube.dto.VisitorLog;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
